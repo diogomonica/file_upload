63a62526f944d0a9ee0ce8dd059909d0adad4d1f
Diogo Monica <diogo@squareup.com>
2012-01-21 22:25:31 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileUpload::Application.config.secret_token = '16ba6ec1307c72f1f700168fb9eee3df3b9acff3d7a89dee7281efb9db600576a8c886fa8663583ac172a1823aa88c37bfd72f3433520976d8239d5898bb0045'

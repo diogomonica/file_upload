1676fa3232fa0673cf4eb79b1a3cad3a1f275bed
Diogo Monica <diogo@squareup.com>
2012-01-28 21:50:54 -0800

    Added upload controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def main
   end
 
+  def upload
+    puts "PARAMS: #{params.inspect}"
+    file = params.delete('file')
+    puts file
+    render :json => "File has been uploaded successfully"
+  end
 end

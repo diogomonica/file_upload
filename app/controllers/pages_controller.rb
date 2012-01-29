298d15e2325e57bdaac0bca1f25a0275b093e834
Diogo Monica <diogo@squareup.com>
2012-01-29 14:43:33 -0800

    WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,14 @@ class PagesController < ApplicationController
   end
 
   def upload
-    puts "PARAMS: #{params.inspect}"
     file = params.delete('file')
-    puts file
+    puts file.inspect
+    puts params.keys.inspect
+    file_name = request.env['HTTP_X_FILE_NAME']
+    Rails.cache.write(file_name, "BLA", :expires_in => 30.minutes)
+    puts "OMGOMG: #{Rails.cache.read(file_name).inspect}"
+    expires_in 30.minutes
+    puts "WTF?"
     render :json => "File has been uploaded successfully"
   end
 end
